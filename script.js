268033724007a5698373a164dba8d4a897777907
Mitchell Glazier <glazierm@gmail.com>
2018-12-31 17:15:07 -0700

    added search function, repos, and ordering

--- script.js
+++ script.js
@@ -1,24 +1,36 @@
 (function() {
-  
+
   var app = angular.module("githubViewer", []);
-  
+
   var MainCtrl = function($scope, $http) {
 
     var onUserComplete = function(response) {
       $scope.user = response.data;
+      $http.get($scope.user.repos_url)
+        .then(onRepos, onError);
+    };
+
+    var onRepos = function(response){
+
+      $scope.repos = response.data;
+
     };
 
     var onError = function(reason) {
-      $scope.error = "Could not find the user";
+      $scope.error = "Could not find the data";
     };
 
-    $http.get("https://api.github.com/users/mitchellglazier")
-      .then(onUserComplete);
+    $scope.search = function(username){
+      $http.get("http://api.github.com/users/" + username)
+      .then(onUserComplete, onError);
+    };
 
-    $scope.message = "DevPoint Coders!"
+    $scope.username = "angular";
+    $scope.message = "Github Viewer!";
+    $scope.repoSortOrder = "-stargazers_count";
 
   };
-  
+
   app.controller("MainCtrl", MainCtrl);
 
 }());
